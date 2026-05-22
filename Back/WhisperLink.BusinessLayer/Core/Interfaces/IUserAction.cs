5d562597c189f4eb1c6f0c85ab60938bfd0a818d
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-22 14:16:06 +0300

    IUserAction interfata pentru actiunile userului

---
+++ Back/WhisperLink.BusinessLayer/Core/Interfaces/IUserAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhisperLink.Domain.Models.Users;
+
+namespace WhisperLink.BusinessLayer.Core.Interfaces
+{
+    public interface IUserAction
+    {
+        Task<IEnumerable<UserDto>> GetAllUsersAsync(string? search = null);
+        Task<UserDto?> GetUserByIdAsync(int id);
+        Task<UserDto?> UpdateUserAsync(int id, UpdateUserDto updateDto);
+        Task<bool> ChangePasswordAsync(int userId, ChangePasswordDto changePasswordDto);
+        Task<bool> DeleteUserAsync(int id);
+    }
+}
