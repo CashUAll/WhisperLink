4b1514b3b1533f08b003de574893cbc8afe38208
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-22 13:43:59 +0300

    EChangePasswordDto pentru functionalitate in caz de stergere a parolei

---
+++ Back/WhisperLink.Domain/Models/Users/EChangePasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhisperLink.Domain.Models.Users
+{
+    // DTO pentru schimbare parolă
+    public class ChangePasswordDto
+    {
+        // Parola actuală pentru confirmare
+        [Required]
+        public string CurrentPassword { get; set; } = string.Empty;
+
+        // Noua parolă, minim 6 caractere
+        [Required]
+        [MinLength(6)]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
