c1c15a3238949c88efe88c45ac1044f091d95a24
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-22 14:14:54 +0300

    IMessageAction interfata pentru actiuni de mesaje

---
+++ Back/WhisperLink.BusinessLayer/Core/Interfaces/IMessageAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhisperLink.Domain.Models.Messages;
+
+namespace WhisperLink.BusinessLayer.Core.Interfaces
+{
+    public interface IMessageAction
+    {
+        Task<MessageDto?> SendMessageAsync(int senderId, SendMessageDto sendMessageDto);
+        Task<IEnumerable<ConversationDto>> GetConversationsAsync(int userId);
+        Task<IEnumerable<MessageDto>> GetConversationWithUserAsync(int userId, int otherUserId);
+        Task<bool> MarkMessageAsReadAsync(int messageId, int userId);
+        Task<bool> DeleteMessageAsync(int messageId, int userId);
+    }
+}
