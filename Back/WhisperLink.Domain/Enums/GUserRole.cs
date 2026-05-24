80eddd7c9f33d410e652319b34b61157adf9873a
George <maharigeorge2005@gmail.com>
2026-05-24 22:56:14 +0300

    GUserRole enumuri pentru autorizarea unui user

---
+++ Back/WhisperLink.Domain/Enums/GUserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace WhisperLink.Domain.Enums
+{
+    // Rolurile utilizatorilor
+    public enum UserRole
+    {
+        User = 0,   // Utilizator normal
+        Admin = 1   // Administrator
+    }
+}
