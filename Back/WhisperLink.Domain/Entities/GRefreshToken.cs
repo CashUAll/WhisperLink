4a5a4bee68d240386961ab3d6c419f18754a2d0a
George <maharigeorge2005@gmail.com>
2026-05-24 22:54:49 +0300

    Clasa GRefreshToken raspunde de regenerarea Tokenurilor

---
+++ Back/WhisperLink.Domain/Entities/GRefreshToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhisperLink.Domain.Entities
+{
+    // Entitatea RefreshToken - pentru JWT authentication
+    public class RefreshToken : BaseEntity
+    {
+        // User-ul căruia îi aparține
+        public int UserId { get; set; }
+
+        // Token-ul propriu-zis (string random base64)
+        public string Token { get; set; } = string.Empty;
+
+        // Data expirării (7 zile)
+        public DateTime ExpiresAt { get; set; }
+
+        // Dacă e revocat (logout)
+        public bool IsRevoked { get; set; } = false;
+        public DateTime? RevokedAt { get; set; }
+        public string? ReplacedByToken { get; set; }
+
+        // Navigation property
+        public User User { get; set; } = null!;
+    }
+}
