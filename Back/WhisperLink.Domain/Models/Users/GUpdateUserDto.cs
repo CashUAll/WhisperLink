6fe909040c236ef30c209536e1861607b53d3ccb
George <maharigeorge2005@gmail.com>
2026-05-24 23:05:41 +0300

    GUpdateUserDto actualizarea profilului dupa necesitate

---
+++ Back/WhisperLink.Domain/Models/Users/GUpdateUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WhisperLink.Domain.Models.Users
+{
+    // DTO pentru actualizare profil utilizator
+    public class UpdateUserDto
+    {
+        // Câmpuri opționale - doar ce vrea user-ul să modifice
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? ProfilePictureUrl { get; set; }
+    }
+}
