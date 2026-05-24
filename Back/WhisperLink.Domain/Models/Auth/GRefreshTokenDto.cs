118f21979f39c061e523fb1805b0739590b8f24d
George <maharigeorge2005@gmail.com>
2026-05-24 22:57:40 +0300

    GRefreshTokenDto deja un token pentru refresh

---
+++ Back/WhisperLink.Domain/Models/Auth/GRefreshTokenDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhisperLink.Domain.Models.Users
+{
+    // DTO pentru refresh token request
+    public class RefreshTokenDto
+    {
+        // Token-ul vechi care expiră
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+    }
+}
