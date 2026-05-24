d956a71c56e4b89cc0ac9b2ce27f33b1b5dca2a6
Alux <alexanfrumosu@mail.ru>
2026-05-24 21:06:32 +0300

    MessageStatud introdus

---
+++ Back/WhisperLink.Domain/Enums/AMessageStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace WhisperLink.Domain.Enums
+{
+    // Statusul mesajelor - pentru read receipts
+    public enum MessageStatus
+    {
+        Sent = 0,       // Mesaj trimis
+        Delivered = 1,  // Mesaj livrat
+        Read = 2        // Mesaj citit
+    }
+}
