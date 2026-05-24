aaf0b0acdf31741c22e246780f73c44fd8c78cb2
Alux <alexanfrumosu@mail.ru>
2026-05-24 21:05:21 +0300

    Friendship si Mesage-ul introdus

---
+++ Back/WhisperLink.Domain/Entities/AFriendship.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WhisperLink.Domain.Enums;
+
+namespace WhisperLink.Domain.Entities
+{
+    // Entitatea Friendship - relațiile de prietenie
+    public class Friendship : BaseEntity
+    {
+        // Cine trimite cererea
+        public int RequesterId { get; set; }
+
+        // Cine primește cererea
+        public int AddresseeId { get; set; }
+
+        // Statusul cererii - Pending, Accepted, Rejected, Blocked
+        public FriendshipStatus Status { get; set; } = FriendshipStatus.Pending;
+
+        // Navigation properties
+        public User Requester { get; set; } = null!;
+        public User Addressee { get; set; } = null!;
+    }
+}
