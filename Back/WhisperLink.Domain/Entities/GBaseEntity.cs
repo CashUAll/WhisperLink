871d21bc68747df2a64d12ec078bb4b1be1da18e
George <maharigeorge2005@gmail.com>
2026-05-24 22:52:03 +0300

    Asta este entitatea de baza unde sunt toate clasele

---
+++ Back/WhisperLink.Domain/Entities/GBaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WhisperLink.Domain.Entities
+{
+    // Clasa de bază pentru toate entitățile - proprietăți comune
+    public abstract class BaseEntity
+    {
+        // ID unic - primary key
+        public int Id { get; set; }
+
+        // Data creării
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Data ultimei modificări
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
