834b483cc35d46d17bfc9cf738dba218c96a6b71
Alux <alexanfrumosu@mail.ru>
2026-05-25 00:04:12 +0300

    automapper

---
+++ Back/WhisperLink.BusinessLayer/Core/Helpers/AAutoMapperProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using WhisperLink.Domain.Entities;
+using WhisperLink.Domain.Models.Friends;
+using WhisperLink.Domain.Models.Messages;
+using WhisperLink.Domain.Models.Users;
+
+namespace WhisperLink.BusinessLayer.Core.Helpers
+{
+    // Configurare AutoMapper - conversii automate Entity ↔ DTO
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            // User mappings
+            CreateMap<User, UserDto>();
+            CreateMap<RegisterDto, User>();
+            CreateMap<UpdateUserDto, User>();
+
+            // Message mappings
+            CreateMap<Message, MessageDto>();
+            CreateMap<SendMessageDto, Message>();
+
+            // Friendship mappings
+            CreateMap<Friendship, FriendshipDto>();
+            CreateMap<SendFriendRequestDto, Friendship>();
+        }
+    }
+}
