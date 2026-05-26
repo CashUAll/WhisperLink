8c8aac1b9e0baaf700584aaff5018101bf136bd7
Scutelniciuc Eduard <eduardscut154@gmail.com>
2026-05-26 23:15:55 +0300

    Remove Backend - Frontend only repository

--- Back/WhisperLink.Domain/Enums/GUserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WhisperLink.Domain.Enums
-{
-    // Rolurile utilizatorilor
-    public enum UserRole
-    {
-        User = 0,   // Utilizator normal
-        Admin = 1   // Administrator
-    }
-}
